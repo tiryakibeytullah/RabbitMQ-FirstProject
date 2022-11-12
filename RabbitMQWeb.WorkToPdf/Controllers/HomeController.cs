c0b726e80ce5ad574fff29089122e042cb2911e3
Beytullah Tiryaki <tiryakibeytullah10@gmail.com>
2022-11-12 17:39:59 +0300

    Sayfa oluşturulması tamamlandı.

---
+++ RabbitMQWeb.WorkToPdf/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RabbitMQWeb.WorkToPdf.Models;
+using System.Diagnostics;
+
+namespace RabbitMQWeb.WorkToPdf.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult ConvertWordToPdf()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ConvertWordToPdf(WordToPdf wordToPdf)
+        {
+            return View();
+        }
+    }
+}
---
+++ RabbitMQWeb.WorkToPdf/Models/WordToPdf.cs
@@ -0,0 +1,8 @@
+﻿namespace RabbitMQWeb.WorkToPdf.Models
+{
+    public class WordToPdf
+    {
+        public string Email { get; set; }
+        public IFormFile WordFile { get; set; }
+    }
+}
