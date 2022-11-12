920cba939336873b6d5e880bcc20ea4e609499a9
Beytullah Tiryaki <tiryakibeytullah10@gmail.com>
2022-11-12 18:07:10 +0300

    RabbitMQ bağlantıları aktif hale getirilip, mesaj gönderme işlemleri tamamlandı.

--- RabbitMQWeb.WorkToPdf/Controllers/HomeController.cs
+++ RabbitMQWeb.WorkToPdf/Controllers/HomeController.cs
@@ -1,16 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
 using RabbitMQWeb.WorkToPdf.Models;
 using System.Diagnostics;
+using System.Text;
 
 namespace RabbitMQWeb.WorkToPdf.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
@@ -36,7 +41,52 @@ namespace RabbitMQWeb.WorkToPdf.Controllers
         [HttpPost]
         public IActionResult ConvertWordToPdf(WordToPdf wordToPdf)
         {
-            return View();
+            try
+            {
+                var factory = new ConnectionFactory();
+                var queueName = "file-queue";
+                var exchangeName = "convert-exchange";
+                var routingKeyName = "word-to-pdf";
+                factory.Uri = new Uri(_configuration.GetConnectionString("RabbitMQ"));
+
+                using (var connection = factory.CreateConnection())
+                {
+                    using (var channel = connection.CreateModel())
+                    {
+                        channel.ExchangeDeclare(exchangeName, ExchangeType.Direct, true, false, null);
+                        channel.QueueDeclare(queueName, true, false, false, null);
+                        channel.QueueBind(queueName, exchangeName, routingKeyName);
+
+                        ResultMessageWorkToPdf resultMessageWorkToPdf = new();
+                        using (MemoryStream stream = new MemoryStream())
+                        {
+                            //Hafızaya kopyala
+                            wordToPdf.WordFile.CopyTo(stream);
+                            resultMessageWorkToPdf.WordByte = stream.ToArray();
+                        }
+
+                        resultMessageWorkToPdf.Email = wordToPdf.Email;
+                        resultMessageWorkToPdf.FileName = Path.GetFileNameWithoutExtension(wordToPdf.WordFile.FileName);
+
+                        string serializeMessage = JsonConvert.SerializeObject(resultMessageWorkToPdf);
+                        var byteResultMessage = Encoding.UTF8.GetBytes(serializeMessage);
+
+                        //Gelen mesajlar rabbitMq restart olsa dahil kalıcı olucak
+                        var properties = channel.CreateBasicProperties();
+                        properties.Persistent = true;
+
+                        channel.BasicPublish(exchangeName, routingKeyName, properties, byteResultMessage);
+
+                        ViewBag.Result = "İlgili Word dosyanız Pdf dosyasına dönüştürme işleminden sonra, Mail adresinize gönderilecektir.";
+
+                        return View();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
         }
     }
 }
---
+++ RabbitMQWeb.WorkToPdf/Models/ResultMessageWorkToPdf.cs
@@ -0,0 +1,9 @@
+﻿namespace RabbitMQWeb.WorkToPdf.Models
+{
+    public class ResultMessageWorkToPdf
+    {
+        public byte[] WordByte { get; set; }
+        public string Email { get; set; }
+        public string FileName { get; set; }
+    }
+}
