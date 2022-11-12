4ed7fc902a91ba262f0d1467cf12e66b9db340cf
Beytullah Tiryaki <tiryakibeytullah10@gmail.com>
2022-11-12 19:47:37 +0300

    RabbitMQ server'ından okunan mesajlar'a tekli şekilde mail gönderilme işlemi kodlandı.

--- RabbitMQ.WorkToPdf.Consumer/Program.cs
+++ RabbitMQ.WorkToPdf.Consumer/Program.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.WorkToPdf.Consumer;
+using Spire.Doc;
 using System.Net;
 using System.Net.Mail;
+using System.Text;
 
 public class Program
 {
     public static bool EmailSend(string email, MemoryStream memory, string fileName)
     {
+        //Notes: GoogleHesap ayarları neticesinde mail gönderme işlemi sırasında Auth. takılmaktayız.
         try
         {
             memory.Position = 0;
@@ -18,7 +24,7 @@ public class Program
             SmtpClient smtpClient = new SmtpClient();
 
             mailMessage.From = new MailAddress("tiryaki52341903@gmail.com");
-            mailMessage.To.Add(fileName);
+            mailMessage.To.Add(email);
             mailMessage.Subject = "Pdf Dosyası Hk.";
             mailMessage.Body = $"{fileName} isimli Word dosyanız, Pdf dosyasına çevirilmiştir. İlgili dosyayı ekte bulabilirsiniz.";
             mailMessage.IsBodyHtml = true;
@@ -29,8 +35,9 @@ public class Program
             smtpClient.Port = 587;
             smtpClient.EnableSsl = true;
 
-            NetworkCredential networkCredential = new NetworkCredential("tiryaki52341903@gmail.com", "19031998beyto");
+            NetworkCredential networkCredential = new NetworkCredential("denemeEmailAdresi", "denemeSifre");
             smtpClient.Credentials = networkCredential;
+            smtpClient.Send(mailMessage);
 
             Console.WriteLine($"Sonuç: {email} adresine mesajınız gönderilmiştir.");
 
@@ -43,6 +50,66 @@ public class Program
         {
             Console.WriteLine($"Exception: {ex.Message}");
             return false;
+        }
+    }
+    private static void Main(string[] args)
+    {
+        var result = false;
+        var factory = new ConnectionFactory();
+        var queueName = "file-queue";
+        var exchangeName = "convert-exchange";
+        var routingKeyName = "word-to-pdf";
+        var rabbitMQClientConnString = "amqps://snahhpmk:oN7FJS_T4LkCBXcZu_pIj0_57DB4PNd5@hawk.rmq.cloudamqp.com/snahhpmk";
+        factory.Uri = new Uri(rabbitMQClientConnString);
+
+        using (var connection = factory.CreateConnection())
+        {
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchangeName, ExchangeType.Direct, true, false, null);
+                channel.QueueBind(queueName, exchangeName, routingKeyName);
+                channel.BasicQos(0, 1, false);
+
+                var consumer = new EventingBasicConsumer(channel);
+                channel.BasicConsume(queueName, false, consumer);
+                consumer.Received += (model, ea) =>
+                {
+                    try
+                    {
+                        Console.WriteLine("Kuyruktan 1 mesaj alındı. İşleniyor..");
+
+                        var deserializeString = Encoding.UTF8.GetString(ea.Body.ToArray());
+                        ResultMessageWordToPdf resultMessage = JsonConvert.DeserializeObject<ResultMessageWordToPdf>(deserializeString);
+
+                        Document document = new Document();
+                        document.LoadFromStream(new MemoryStream(resultMessage.WordByte), FileFormat.Docx2013);
+
+                        using (var memoryStream = new MemoryStream())
+                        {
+                            document.SaveToStream(memoryStream, FileFormat.PDF);
+                            result = EmailSend(resultMessage.Email, memoryStream, resultMessage.FileName);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Exception: {ex.Message}");
+                        throw;
+                    }
+
+                    if (result)
+                    {
+                        //Dönüştürme ve mail gönderme işlemi başarılı ise, RabbitMQ'dan mesaj silinir.
+                        Console.WriteLine("Kuyruktan okunan mesaj başarılı bir şekilde işlenmiştir.");
+                        channel.BasicAck(ea.DeliveryTag, false);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Kuyruktan okunan mesaj sırasında bir hata meydana geldi.");
+                    }
+                };
+                Console.WriteLine("Çıkmak için bir tuşa tıklayınız..");
+                Console.ReadLine();
+            }
         }
     }
 }
---
+++ RabbitMQ.WorkToPdf.Consumer/ResultMessageWordToPdf.cs
@@ -0,0 +1,9 @@
+﻿namespace RabbitMQ.WorkToPdf.Consumer
+{
+    public class ResultMessageWordToPdf
+    {
+        public byte[] WordByte { get; set; }
+        public string Email { get; set; }
+        public string FileName { get; set; }
+    }
+}
